c6f94484d5569d9fb5345712b245560ba2d40d61
SergioTrujilloDev <sergio.trujillo@dxc.com>
2022-07-20 19:57:45 -0500

    Accommodate home page styles
    Add init basic structure to home page and basic styles.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import homePageSyles from "./HomePage.module.css"
 
 function HomePage() {
   return (
-    <div>
-        <h1>Home page</h1>
-    </div>
+    <Container maxWidth="xl" className={homePageSyles.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} >
+          Icon
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5">
+            The smartest flight search on the internet
+          </Typography>
+        </Grid>
+        <Grid item xs={12} container justifyContent="space-around">
+          <Grid item xs={6}>
+            Trip type
+          </Grid>
+          <Grid item xs={6} container justifyContent="flex-end">
+            Passengers
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          Search Form
+        </Grid>
+        <Grid item xs={12} container justifyContent="center">
+          Button
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
