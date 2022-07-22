5ebd7ca71a5e8f98ca9b8c2b5d432332fdc2b200
SergioTrujilloDev <sergio.trujillo@dxc.com>
2022-07-22 11:51:20 -0500

    Add formik
    Install formik and add to home page form.

--- src/components/roundSelect.tsx
+++ src/components/roundSelect.tsx
@@ -1,17 +1,25 @@
-import { FormControl, Select, MenuItem } from '@mui/material';
+import { FormControl, Select, MenuItem, Alert } from '@mui/material';
 
-export default function roundSelect({value, optionName, handleChange, arr}:
-    {value: string | number, optionName: string, handleChange: Function, arr: string[] | number[]}
+export default function roundSelect({formik, optionName, arr}:
+    {formik: any, optionName: string, arr: string[] | number[],
+    }
     ){
     return(
         <FormControl fullWidth>
             <Select
-            value={value}
-            onChange={(e) => handleChange(e, optionName)}
-            style={{borderRadius: "30px", height: "35px"}}
+                name={optionName}
+                value={formik.values[optionName]}
+                onChange={formik.handleChange}
+                onBlur={formik.onBlur}
+                style={{borderRadius: "30px", height: "35px"}}
             >
                 {arr.map((item) => <MenuItem key={item} value={item}>{item}</MenuItem>)}
             </Select>
+            {formik.errors[optionName] && formik.touched[optionName] ? 
+              <Alert severity="error" style={{marginTop: "20px"}}>
+                {formik.errors[optionName]}
+            </Alert> 
+            : null}
         </FormControl>
     )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,53 +1,54 @@
-import { Container, Grid, Typography, SelectChangeEvent } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import homePageSyles from "./HomePage.module.css"
-import {useState} from "react"
 import RoundSelect from "../../components/roundSelect"
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 function HomePage() {
-  const [flightInfo, setFlightInfo] = useState<{
-    tripType: string,
-  }>({
-    tripType: "Round Trip",
+  const formik = useFormik({
+    validationSchema: Yup.object().shape({
+      tripType: Yup.string().required("Trip type is required!")
+    }),
+    initialValues: {
+      tripType: 'Round Trip'
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
-
-  const handleChange = (event: SelectChangeEvent, optionName: string ) => {
-    setFlightInfo({
-      ...flightInfo,
-      [optionName]: event.target.value as string
-    })
-  };
   
   return (
     <Container maxWidth="xl" className={homePageSyles.container}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} >
-          Icon
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h5">
-            The smartest flight search on the internet
-          </Typography>
-        </Grid>
-        <Grid item xs={12} container justifyContent="space-between" spacing={2}>
-          <Grid item xs={5} md={2}>
-            <RoundSelect 
-              value={flightInfo.tripType}
-              optionName="tripType"
-              handleChange={handleChange}
-              arr={["Round Trip", "One Way"]}
-            />
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} >
+            Icon
           </Grid>
-          <Grid item xs={6} md={2}>
-          passenger
+          <Grid item xs={12}>
+            <Typography variant="h5">
+              The smartest flight search on the internet
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container justifyContent="space-between" spacing={2}>
+            <Grid item xs={5} md={2}>
+              <RoundSelect
+                formik={formik}
+                optionName="tripType"
+                arr={["Round Trip", "One Way"]}
+              />
+            </Grid>
+            <Grid item xs={6} md={2}>
+            passenger
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            Search Form
+          </Grid>
+          <Grid item xs={12} container justifyContent="center">
+            <button type="submit">Search</button>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          Search Form
-        </Grid>
-        <Grid item xs={12} container justifyContent="center">
-          Button
-        </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 }
