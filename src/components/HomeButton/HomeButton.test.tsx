352b54b4af33eb37a96450fea2319c5b782363bc
Ariadna Gutierrez <a.gutierrezgovea@dxc.com>
2022-07-28 15:51:58 -0500

    Unit Testing HomeButton

--- src/components/HomeButton/HomeButton.test.tsx
+++ src/components/HomeButton/HomeButton.test.tsx
@@ -1,29 +1,42 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import HomeButton from "./HomeButton";
-import { MemoryRouter } from "react-router-dom";
+import { render, screen, fireEvent } from '@testing-library/react';
+import HomeButton from './HomeButton';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 const mockedNavigator = jest.fn();
 
-jest.mock("react-router-dom", () => ({
-  ...(jest.requireActual("react-router-dom") as any),
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
   useNavigate: () => mockedNavigator,
 }));
 
-describe("HomeButton component", () => {
-  test("renders correctly", () => {
+describe('HomeButton component', () => {
+  test('renders correctly', () => {
     render(<HomeButton />);
 
-    expect(screen.getByTestId("home-button")).toBeInTheDocument();
+    expect(screen.getByTestId('home-button')).toBeInTheDocument();
   });
 
-  test("navigates to /", () => {
+  test('do nothing when isHomePage props is truthy', () => {
+    const history = createMemoryHistory();
     render(
-      <MemoryRouter>
+      <Router location={'/'} navigator={history}>
+        <HomeButton isHomePage />
+      </Router>
+    );
+
+    fireEvent.click(screen.getByTestId('home-button'));
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('navigates to /', () => {
+    render(
+      <MemoryRouter initialEntries={['/results']}>
         <HomeButton />
       </MemoryRouter>
     );
 
-    fireEvent.click(screen.getByTestId("home-button"));
-    expect(mockedNavigator).toHaveBeenCalledWith("/");
+    fireEvent.click(screen.getByTestId('home-button'));
+    expect(mockedNavigator).toHaveBeenCalledWith('/');
   });
 });
--- src/pages/ResultsPage/ResultsPage.tsx
+++ src/pages/ResultsPage/ResultsPage.tsx
@@ -1,15 +1,38 @@
 import { Container, Grid } from "@mui/material";
-import { HomeButton } from "../../components/index";
+import { HomeButton, RoundedSelect } from "../../components/index";
 import { StyledEngineProvider } from "@mui/material/styles";
 import resultsPageStyles from "./ResultsPage.module.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import constants from "../../utils/constants";
 
 function ResultsPage() {
+  const formik = useFormik({
+    validationSchema: Yup.object().shape({
+      tripType: Yup.string().required("Trip type is required!"),
+      passengers: Yup.number().required("Number of passengers is required!"),
+    }),
+    initialValues: {
+      tripType: constants.tripType[0],
+      passengers: 1,
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <StyledEngineProvider injectFirst>
       <Container maxWidth="xl" className={resultsPageStyles.container}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <HomeButton />
+          </Grid>
+          <Grid item xs={5} md={4}>
+            <RoundedSelect
+              formik={formik}
+              optionName="tripType"
+              options={constants.tripType}
+            />
           </Grid>
         </Grid>
       </Container>
