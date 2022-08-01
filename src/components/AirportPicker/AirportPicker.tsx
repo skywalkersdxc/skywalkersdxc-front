41335ba550b7e0025f131743cbfaaf2d6a2f82ac
Ariadna Gutierrez <a.gutierrezgovea@dxc.com>
2022-08-01 17:40:54 -0500

    Inputs styles

---
+++ src/components/AirportPicker/AirportPicker.tsx
@@ -0,0 +1,57 @@
+import { Alert, Autocomplete, FormControl, TextField } from "@mui/material";
+import { Place, AirplanemodeActive } from "@mui/icons-material";
+import React from "react";
+import styles from "./AirportPicker.module.css";
+
+interface AirportPickerProps {
+  airports: any[];
+  flightType: string;
+}
+
+export interface IconComponentProps {
+  type: string | undefined;
+}
+
+const AirportPicker: React.FC<AirportPickerProps> = ({
+  airports,
+  flightType,
+}: AirportPickerProps) => {
+  const handleDateChange = (keyboardInput: string | undefined) => {};
+
+  const IconComponent: React.FC<IconComponentProps> = ({ type }) => {
+    switch (type) {
+      case "departure":
+        return <Place className={styles.icon} />;
+      case "destination":
+        return <AirplanemodeActive className={styles.icon} />;
+      default:
+        return null;
+    }
+  };
+
+  const selectLabel = (type: string) => {
+    switch (type) {
+      case "departure":
+        return "From";
+      case "destination":
+        return "To";
+      default:
+        return null;
+    }
+  };
+  return (
+    <FormControl fullWidth className={styles.container}>
+      <IconComponent type={flightType} />
+      <Autocomplete
+        disablePortal
+        options={airports}
+        renderInput={(params) => (
+          <TextField {...params} label={selectLabel(flightType)} />
+        )}
+        className={styles.input}
+      />
+    </FormControl>
+  );
+};
+
+export default AirportPicker;
