f29b055af9a012d1d8dda35ac33d6025a912dc8b
Ariadna Gutierrez <a.gutierrezgovea@dxc.com>
2022-08-02 14:20:39 -0500

    Unit Testing Home Button missing test for 100

--- src/components/HomeButton/HomeButton.test.tsx
+++ src/components/HomeButton/HomeButton.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import HomeButton from "./HomeButton";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 const mockedNavigator = jest.fn();
 
@@ -16,9 +17,21 @@ describe("HomeButton component", () => {
     expect(screen.getByTestId("home-button")).toBeInTheDocument();
   });
 
+  test("do nothing when isHomePage props is truthy", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={"/"} navigator={history}>
+        <HomeButton isHomePage />
+      </Router>
+    );
+
+    fireEvent.click(screen.getByTestId("home-button"));
+    expect(history.location.pathname).toBe("/");
+  });
+
   test("navigates to /", () => {
     render(
-      <MemoryRouter>
+      <MemoryRouter initialEntries={["/results"]}>
         <HomeButton />
       </MemoryRouter>
     );
