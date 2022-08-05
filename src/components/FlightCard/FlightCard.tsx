6914226359dfdb51caa81c416a2dce8926398bb7
SergioTrujilloDev <sergio.trujillo@dxc.com>
2022-08-05 11:19:59 -0500

    Move flight info component
    Move flight info component to its own file

--- src/components/FlightCard/FlightCard.tsx
+++ src/components/FlightCard/FlightCard.tsx
@@ -1,38 +1,7 @@
-import { FlightResultsProps, ItinerariesProps } from "../../pages/HomePage/interfaces";
-import { Button, Typography, Box, LinearProgress, Grid } from "@mui/material";
-import { convertDate, timeTravelDiff } from "../../utils/utils"
+import { FlightResultsProps,  } from "../../pages/HomePage/interfaces";
+import { Button, Grid } from "@mui/material";
 import flightCardStyles from "./FlightCard.module.css"
-
-const aaImgUrlDummy = "https://www.aa.com/content/images/homepage/mobile-hero/en_US/Airplane-1.png"
-
-export const FlightInfoComponent: React.FC<{itineraries: ItinerariesProps}> = ({itineraries}: {itineraries: ItinerariesProps}) => {
-    const {departure, arrival} = itineraries
-    return (
-        <Grid item xs={12} container data-testid="flightInfoComponent">
-            <Grid item xs={2} container justifyContent="center" alignItems="center">
-                <img alt="airlineIcon" src={aaImgUrlDummy} width={40}/>
-            </Grid>
-            <Grid item xs={10} container className={flightCardStyles.originDestiny}>
-                <Grid item xs={12} container justifyContent="space-between">
-                    <Typography variant={"body1"}>{convertDate(departure.at)}</Typography>
-                    <Typography variant={"body1"}>{convertDate(arrival.at)}</Typography>
-                </Grid>
-                <Grid item xs={12} container justifyContent="space-between">
-                    <Typography variant={"body1"}>{departure.iataCode}</Typography>
-                    <Typography variant={"body1"}>
-                        {timeTravelDiff(departure.at, arrival.at)}
-                    </Typography>
-                    <Typography variant={"body1"}>{arrival.iataCode}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box sx={{ width: '100%' }}>
-                        <LinearProgress variant="determinate" value={100} className={flightCardStyles.flightProgress}/>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Grid>
-    )
-}
+import FlightInfoComponent from "./FlightInfoComponent";
 
 const FlightCard: React.FC<{flightResults: FlightResultsProps}> = ({flightResults} : {flightResults: FlightResultsProps}) => {
    return (
