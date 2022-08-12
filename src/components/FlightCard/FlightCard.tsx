fc0f414b44be03829ee256a8b78199683ab9141c
SergioTrujilloDevDxc <sergio.trujillo@dxc.com>
2022-08-12 09:56:58 -0500

    Fix key error
    Fix key error using random string function and unique parameters for each element

--- src/components/FlightCard/FlightCard.tsx
+++ src/components/FlightCard/FlightCard.tsx
@@ -4,18 +4,19 @@ import flightCardStyles from "./FlightCard.module.css"
 import FlightInfoComponent from "./FlightInfoComponent";
 
 const FlightCard: React.FC<{flightResults: FlightResultsProps}> = ({flightResults} : {flightResults: FlightResultsProps}) => {
+    let randomString = (Math.random() + 1).toString(36).substring(7);
    return (
-    <Grid data-testid="flightCard" key={flightResults.id} container item className={flightCardStyles.containerCard} xs={12}>
-        {flightResults.itineraries.map((item) => <FlightInfoComponent key={item.duration} itineraries={item.segments[0]}/>)}
+    <Grid data-testid="flightCard" key={randomString + flightResults.id} container item className={flightCardStyles.containerCard} xs={12}>
+        {flightResults.itineraries.map((item) => <FlightInfoComponent key={randomString + item.segments[0].arrival.at + flightResults.id} itineraries={item.segments[0]}/>)}
         <Grid item xs={12} container flexDirection="row" className={flightCardStyles.amountContainer}>
-        <Grid item xs={12}>
-            <hr className={flightCardStyles.dividerAmount}/>
-        </Grid>
-        <Grid item xs={12} container justifyContent="flex-end">
-            <Grid item xs={4} container justifyContent="flex-end">
-                <Button variant="outlined" className={flightCardStyles.amountButton}>${flightResults.price.total}</Button>
+            <Grid item xs={12}>
+                <hr className={flightCardStyles.dividerAmount}/>
             </Grid>
-        </Grid>
+            <Grid item xs={12} container justifyContent="flex-end">
+                <Grid item xs={4} container justifyContent="flex-end">
+                    <Button variant="outlined" className={flightCardStyles.amountButton}>${flightResults.price.total}</Button>
+                </Grid>
+            </Grid>
         </Grid>
     </Grid>
    )
