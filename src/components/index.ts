1d4d65aea4b14ecc9922b0b34394f22ebe6737be
“h-franco-dxc” <hector.franco@dxc.com>
2022-08-19 12:33:28 -0500

    Updates

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as RoundedSelect } from './RoundedSelect/roundedSelect';
-export { default as HomeButton } from './HomeButton/HomeButton';
-export { default as DatePicker } from './DatePicker/DatePicker';
-export { default as AirportPicker } from './AirportPicker/AirportPicker';
-export { default as SubmitButton } from "./SubmitButton/submitButton";
-export { default as FlightCard } from './FlightCard/FlightCard';
